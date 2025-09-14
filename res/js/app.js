bbb74618e30bf56daf50bc7a88bc03981bbd3bd9
VoidSplit <46133265+VoidSplit@users.noreply.github.com>
2025-09-14 22:03:09 +0200

    add a tree

--- res/js/app.js
+++ res/js/app.js
@@ -2,7 +2,7 @@
 const catalog_content = document.getElementById('catalog_content')
 
 // --- CONFIG ---
-const NB_FILES = 25
+const NB_FILES = 26
 const BLACKLIST = new Set([])
 const LOCAL = false
 
