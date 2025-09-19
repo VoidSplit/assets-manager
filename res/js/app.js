4c46370a25bda2709ee9db67dbe60a8d72912eb9
VoidSplit <46133265+VoidSplit@users.noreply.github.com>
2025-09-19 11:08:24 +0200

    adding 0032

--- res/js/app.js
+++ res/js/app.js
@@ -2,7 +2,7 @@
 const catalog_content = document.getElementById('catalog_content')
 
 // --- CONFIG ---
-const NB_FILES = 31
+const NB_FILES = 32
 const BLACKLIST = new Set([])
 const LOCAL = false
 
