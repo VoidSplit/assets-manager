acfe875107c75d55f7bbadc6038eb022107a836f
VoidSplit <46133265+VoidSplit@users.noreply.github.com>
2026-04-12 12:22:44 +0200

    adding avif format

--- src/components/grid_card/Grid_card.jsx
+++ src/components/grid_card/Grid_card.jsx
@@ -5,7 +5,7 @@ export function Grid_card({cardId, cardName, onClick, isNew}) {
     return(
         <div className="grid_card" onClick={onClick}>
             {isNew && <div className='new_indicator'>Nouveau</div>}
-            <img src={`https://voidsplit.github.io/assets-manager/src/assets/images/original/${cardId}.png`} alt={`Image representant un ${cardName}`} />
+            <img loading="lazy" src={`https://voidsplit.github.io/assets-manager/src/assets/images/avif/${cardId}.avif`} alt={`Image representant un ${cardName}`} />
             <div className="infos">
                 <div className="name">{cardName}</div>
                 <span className="id">#{cardId}</span>
