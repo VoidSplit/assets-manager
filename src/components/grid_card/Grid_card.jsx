d2b1d4ef0df79195a86856f7ecebe5a68ded1c0f
VoidSplit <46133265+VoidSplit@users.noreply.github.com>
2026-04-12 15:53:43 +0200

    fixing images

--- src/components/grid_card/Grid_card.jsx
+++ src/components/grid_card/Grid_card.jsx
@@ -5,7 +5,7 @@ export function Grid_card({cardId, cardName, onClick, isNew}) {
     return(
         <div className="grid_card" onClick={onClick}>
             {isNew && <div className='new_indicator'>Nouveau</div>}
-            <img fetchpriority="high" src={`../../src/assets/images/avif/optimized/${cardId}.avif`} alt={`Image representant un ${cardName}`} />
+            <img fetchpriority="high" src={`./src/assets/images/avif/optimized/${cardId}.avif`} alt={`Image representant un ${cardName}`} />
             <div className="infos">
                 <div className="name">{cardName}</div>
                 <span className="id">#{cardId}</span>
